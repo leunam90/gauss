73278541f90b82de4467999b5fb1e30e4a2ba58c
Octavio Quiroz <octavioquiroz30@gmail.com>
2020-10-28 18:03:12 -0600

    se eliminar referencia a archivo .bat estatico

--- commands/createproject.js
+++ commands/createproject.js
@@ -12,7 +12,7 @@ let execp = util.promisify(exec);
 module.exports = {
     suiteapp: async (project) => {
         try {
-            let { stdout, stderr } = await execp(path.join(_routes.sdfcli, `sdfcli.bat createproject -type ${project.type} -parentdirectory ${_routes.cwd} -projectname ${project.name} -projectid ${project.projectid} -publisherid ${project.publisherid} -projectversion ${project.projectversion}`));
+            let { stdout, stderr } = await execp(path.join(_routes.sdfcli, `sdfcli createproject -type ${project.type} -parentdirectory ${_routes.cwd} -projectname ${project.name} -projectid ${project.projectid} -publisherid ${project.publisherid} -projectversion ${project.projectversion}`));
             if (stdout.match(/.+ has been created/)) {
                 //when the suiteapp is created then modify the manifest to add default features
                 const manifest = `<manifest projecttype="${project.type}">
